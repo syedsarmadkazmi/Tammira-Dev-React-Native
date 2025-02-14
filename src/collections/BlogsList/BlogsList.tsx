639fa254d6843a2b9ec8c6d90706029630e550ae
Sarmad Kazmi <sarmad.abbas@tentwenty.me>
2025-02-14 23:08:30 +0500

    feat: created collections BlogList and TagsList, defined theme colors

--- src/collections/BlogsList/BlogsList.tsx
+++ src/collections/BlogsList/BlogsList.tsx
@@ -1,10 +1,20 @@
-import { Text, View } from 'react-native';
 import React from 'react';
+import { FlatList } from 'react-native';
+import { useSelector } from 'react-redux';
+import { RootState } from '~app/store';
+import { Blog } from '~app/types';
+import { BlogCard } from '~app/components';
+import { styles } from './styles';
 
-export const BlogsList = () => {
+export const BlogList: React.FC = () => {
+  const blogs = useSelector((state: RootState) => state.blog.blogs);
+
   return (
-    <View>
-      <Text>BlogsList</Text>
-    </View>
+    <FlatList
+      data={blogs}
+      keyExtractor={(item: Blog) => item._id}
+      renderItem={({ item }) => <BlogCard blog={item} />}
+      contentContainerStyle={styles.list}
+    />
   );
 };
--- src/collections/TagsList/TagsList.tsx
+++ src/collections/TagsList/TagsList.tsx
@@ -1,10 +1,18 @@
-import { Text, View } from 'react-native';
 import React from 'react';
+import { ScrollView } from 'react-native';
+import { useSelector } from 'react-redux';
+import { RootState } from '~app/store';
+import { Tag } from '~app/components';
+import { styles } from './styles';
 
-export const TagsList = () => {
+export const TagList: React.FC = () => {
+  const availableTags = useSelector((state: RootState) => state.blog.availableTags);
+
   return (
-    <View>
-      <Text>TagsList</Text>
-    </View>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.container}>
+      {availableTags.map((tag) => (
+        <Tag key={tag} tag={tag} />
+      ))}
+    </ScrollView>
   );
 };
